0e1490528b78888d0f3309c5ea710c2006f3d1c2
Snigdhajyoti Ghosh <snigdhajyoti.ghosh@gmail.com>
2019-04-04 19:47:51 +0530

    Make use of output file

--- MagicNumbers.py
+++ MagicNumbers.py
@@ -6,21 +6,33 @@ noOfGeneration = 2000  # type: int
 mutationRate = 0.2  # type: float
 crossoverRate = 0.2  # type: float
 populationSize = 300  # type: int
-seedRange = 5  # type: int
+seedRangeMin = 1  # type: int
+seedRangeMax = 5  # type: int
 
 
 def constant_to_list():
     return "List Length: {}\nPower Of Equation: {}\nRange Min: {}, Max: {}\nNo Of Generation: {}\n" \
-           "Mutation Rate: {}\nCrossover Rate: {}\nPopulation Size: {}\nSeed Range: {}\n\n" \
+           "Mutation Rate: {}\nCrossover Rate: {}\nPopulation Size: {}\nSeed Range: {} - {}\n\n" \
         .format(listLength, powerOfEquation, rangeMin, rangeMax, noOfGeneration, mutationRate,
-                crossoverRate, populationSize, seedRange)
+                crossoverRate, populationSize, seedRangeMin, seedRangeMax)
 
 
 def get_file_name():
     return 'out{}.txt'.format(hash(constant_to_list()))
 
 
-def write_to_file(file_content):
+def make_table(seed_value, genomeList):
+    equation_as_string = '{}'.format(genomeList[0])
+    for i in range(1, len(genomeList) - 1):
+        equation_as_string += ' + {}'.format(genomeList[i])
+    equation_as_string += ' = {}'.format(genomeList[len(genomeList) - 1])
+    return '\n{}\t\t\t\t{}'.format(seed_value, equation_as_string)
+
+
+def write_to_file(all_output):
+    file_content = ''
+    for (seed_value, genome_list) in all_output:
+        file_content += make_table(seed_value, genome_list)
     string_to_update_file = constant_to_list()
     header = 'Seed Value\t\tpoints\n-------------------------------'
     file_location = 'output/' + get_file_name()
@@ -34,11 +46,3 @@ def read_and_print_file():
     file_obj = open(file_location, 'r')
     print(file_obj.read())
     file_obj.close()
-
-
-def make_table(seed_value, genomeList):
-    equation_as_string = '{}'.format(genomeList[0])
-    for i in range(1, len(genomeList) - 1):
-        equation_as_string += ' + {}'.format(genomeList[i])
-    equation_as_string += ' = {}'.format(genomeList[len(genomeList) - 1])
-    return '\n{}\t\t\t\t{}'.format(seed_value, equation_as_string)
--- PowerOfConjustureMain.py
+++ PowerOfConjustureMain.py
@@ -1,16 +1,21 @@
 from random import seed
 import PowerOfConjusture
-from MagicNumbers import make_table, read_and_print_file, write_to_file, seedRange
+from MagicNumbers import read_and_print_file, write_to_file, seedRangeMax, seedRangeMin
+
+
+def find_equation():
+    all_outputs = []
+    for seedValue in range(seedRangeMin, seedRangeMax + 1):
+        seed(seedValue)
+        best = PowerOfConjusture.get_best_individual()
+        if best.score == 0.0:
+            all_outputs.append((seedValue, best.genomeList))
+    write_to_file(all_outputs)
+    read_and_print_file()
+
 
 if __name__ == "__main__":
     try:
         read_and_print_file()
     except IOError:
-        output_string = ''
-        for seedValue in range(1, seedRange + 1):
-            seed(seedValue)
-            best = PowerOfConjusture.get_best_individual()
-            if best.score == 0.0:
-                output_string += make_table(seedValue, best.genomeList)
-        write_to_file(output_string)
-        read_and_print_file()
+        find_equation()
